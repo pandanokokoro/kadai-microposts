8c94461ce110d811dcc17a372808edc888396ae3
yoichi.mihara <pandanokokoro41@gmail.com>
2022-11-29 02:01:56 +0000

    top page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..20).each do |number|
+  User.create(name: "test_name_" + number.to_s, email: "test" + number.to_s + "@examle.com", password: "test_000" + number.to_s)
+end
