4b839ba8c224b85a620399e69d0d59eb948d3d4e
yoichi.mihara <pandanokokoro41@gmail.com>
2022-12-21 09:34:17 +0000

    mod micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_many :favorites
   
+  has_many :favorite_users, through: :favorites, source: :user
+
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
