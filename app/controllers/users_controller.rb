47ae2e93fbfbda6964bd0db80a04df033ff5c3b6
yoichi.mihara <pandanokokoro41@gmail.com>
2022-12-21 07:08:26 +0000

    mod micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @pagy, @microposts = pagy(@user.favorite_posts)
+    counts(@user)
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,22 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
-  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
-  
   has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  has_many :favorites
+  has_many :favorite_posts, through: :favorites, source: :micropost
+
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
     end
+  end
+  
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
   end
   
   def unfollow(other_user)
@@ -25,13 +30,24 @@ class User < ApplicationRecord
     relationship.destroy if relationship
   end
   
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def favorite_post?(micropost)
+    self.favorite_posts.include?(micropost)
   end
   
   def feed_microposts
     puts Micropost.where(user_id: self.following_ids + [self.id])
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+
   
 end
